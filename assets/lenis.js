c52fa247f07e24754f65f2b1177947378d9f6bb1
Sohail <sohailkhan21265285@gmail.com>
2026-07-14 10:47:06 +0500

    s

--- assets/lenis.js
+++ assets/lenis.js
@@ -2,7 +2,7 @@ if (!window.matchMedia('(prefers-reduced-motion: reduce)').matches) {
   window.lenis = new Lenis({
     duration: 1,
     smoothWheel: true,
-    wheelMultiplier: 0.4
+  
   });
 
   function raf(time) {
